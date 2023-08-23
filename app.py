f97f25b76a1e05b67023c622614e0c9e47d0677e
matthewrellwood <matthew.r.ellwood@gmail.com>
2023-08-23 13:24:55 +0100

    initail commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from extras import apology, login_required, GBP
+
+# Configure application
+app = Flask(__name__, static_folder='static')
+
+# Custom filter
+app.jinja_env.filters["GBP"] = GBP
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///aepricelist.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Show Home Page"""
+    if request.method == "GET":
+        return render_template ("index.html")
+    else:
+        return render_template("customer_order.html")
