df0bf8813a49ef98f86a71901115d81d4f74bdca
matthewrellwood <matthew.r.ellwood@gmail.com>
2023-08-26 16:10:39 +0100

    secondry stages

--- app.py
+++ app.py
@@ -39,4 +39,61 @@ def index():
     if request.method == "POST":
         return render_template ("index.html")
     else:
-        return render_template("customer_order.html")
+        return render_template("index.html")
+
+
+@app.route("/orders", methods=["GET", "POST"])
+def orders():
+    """Show order Page"""
+    if request.method == "POST":
+        return render_template ("orders.html")
+    else:
+        return render_template("orders.html")
+
+
+@app.route("/list_of_orders", methods=["GET", "POST"])
+def list_of_orders():
+    # do this
+    if request.method == "POST":
+        # do this
+        order_no = request.form.get("order_no")
+        return render_template("stock_list.html", order_no = order_no)
+    else:
+        ord_detail=db.execute("select * from orders;")
+        #ord_detail = db.execute("select * from orders join customers on orders.cust_id = customers.id;")
+        #return render_template("list_of_orders.html", ord_detail = ord_detail)
+        return render_template("list_of_orders.html", ord_detail = ord_detail)
+
+
+@app.route("/stock_list", methods=["GET", "POST"])
+def stock_list():
+        if request.method == "POST":
+        # do this
+        
+            stock = db.execute("SELECT * FROM stock;")
+            return render_template("stock_list.html")
+        else:
+            stock = db.execute("SELECT * FROM stock;")
+            return render_template("stock_list.html", stock = stock)
+
+@app.route("/lounge.html", methods=["GET", "POST"])
+def lounge():
+        if request.method == "POST":
+        # do this
+            return render_template("lounge.html")
+        else:
+            lounge = db.execute("SELECT * FROM stock WHERE Type = 'lounge' ;")
+            return render_template("lounge.html", lounge = lounge)
+
+
+@app.route("/bedroom.html", methods=["GET", "POST"])
+def bedroom():
+        if request.method == "POST":
+        # do this
+            bedroom = db.execute("SELECT * FROM stock WHERE Type = 'Bedroom' ;")
+            return render_template("bedroom.html", bedroom = bedroom) 
+            #return render_template("stock_list.html")
+        else:
+            bedroom = db.execute("SELECT * FROM stock WHERE Type = 'Bedroom' ;")
+            return render_template("bedroom.html", bedroom = bedroom)  
+    
